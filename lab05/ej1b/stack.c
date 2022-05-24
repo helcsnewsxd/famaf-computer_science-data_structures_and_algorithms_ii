4e2545700b2fc548a198ebaa5a543bf6518fa0e6
EmanuelHerrador <emanuel.nicolas.herrador@mi.unc.edu.ar>
2022-05-24 17:45:40 -0300

    arreglado stack menos memoria

--- lab05/ej1b/stack.c
+++ lab05/ej1b/stack.c
@@ -3,19 +3,25 @@
 #include <assert.h>
 #include "stack.h"
 
+typedef struct _stack_node *nodo;
+
 struct _s_stack{
+    nodo pila;
+    unsigned int size;
+};
+
+struct _stack_node{
 	stack_elem elem;
-	stack next;
-	unsigned int size;
+	nodo next;
 };
 
 void stack_debug(stack s){
-	int *a = stack_to_array(s);
-	size_t length = stack_size(s);
-	printf("Size = %lu\n",length);
-	for(unsigned int i=0;i<length;i++){printf("(%d)-",a[i]);}
-	printf("\n=========================\n\n");
-	free(a);
+    int *a = stack_to_array(s);
+    size_t length = stack_size(s);
+    printf("Size = %lu\n",length);
+    for(unsigned int i=0;i<length;i++){printf("(%d)-",a[i]);}
+    printf("\n=========================\n\n");
+    free(a);
 }
 
 bool stack_invrep(stack s){
@@ -23,54 +29,59 @@ bool stack_invrep(stack s){
 }
 
 stack stack_empty(){
-	return NULL;
+	stack nuevo_stack = malloc(sizeof(struct _s_stack));
+    nuevo_stack->size = 0;
+    nuevo_stack->pila = NULL;
+    return nuevo_stack;
 }
 
 stack stack_push(stack s, stack_elem e){
-//	printf("Antes\n");
-//	stack_debug(s);
+	printf("Antes\n");
+	stack_debug(s);
 	assert(stack_invrep(s));
-	stack aux = malloc(sizeof(struct _s_stack));
-	aux->elem = e;
-	aux->next = s;
-	s = aux;
 
-	if(stack_is_empty(s->next)){ s->size = 1; }
-	else{ s->size = (s->next)->size + 1; }
+    s->size++;
+    nodo aux = malloc(sizeof(struct _stack_node));
+	aux->elem = e;
+	aux->next = s->pila;
+	s->pila = aux;
+
 	assert(stack_invrep(s));
-//	printf("Despues\n");
-//	stack_debug(s);
+	printf("Despues\n");
+	stack_debug(s);
 	return s;
 }
 
 stack stack_pop(stack s){
 	assert(!stack_is_empty(s));
 	assert(stack_invrep(s));
-//	printf("Antes\n");
-//	stack_debug(s);
-	stack aux = s->next;
-	free(s);
-	s = aux;
-//	printf("Despues\n");
-//	stack_debug(s);
+	printf("Antes\n");
+	stack_debug(s);
+	
+    nodo aux = (s->pila)->next;
+	free(s->pila);
+	s->pila = aux;
+	s->size--;
+
+	printf("Despues\n");
+	stack_debug(s);
 	assert(stack_invrep(s));
 	return s;
 }
 
 unsigned int stack_size(stack s){
 	assert(stack_invrep(s));
-	if(stack_is_empty(s))return 0;
 	return s->size;
 }
 
 stack_elem stack_top(stack s){
 	assert(!stack_is_empty(s));
 	assert(stack_invrep(s));
-	return s->elem;
+	return (s->pila)->elem;
 }
 
 bool stack_is_empty(stack s){
-	return s==NULL;
+	return s!=NULL && s->size==0;
 }
 
 stack_elem *stack_to_array(stack s){
@@ -78,9 +89,9 @@ stack_elem *stack_to_array(stack s){
 	unsigned int length = stack_size(s);
 	if(length == 0)return NULL;
 	stack_elem *array = calloc(length,sizeof(stack_elem));
-	stack act = s;
+	nodo act = s->pila;
 	length--;
-	while(!stack_is_empty(act)){
+	while(act != NULL){
 		array[length] = act->elem;
 		act=act->next;
 		length--;
@@ -93,5 +104,8 @@ stack stack_destroy(stack s){
 	while(!stack_is_empty(s)){
 		s = stack_pop(s);
 	}
+    free(s->pila);
+    free(s);
+    s = NULL;
 	return s;
 }
